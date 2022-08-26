e8137cfee832e2a7bb5097dc3dc933699bd3f9ad
Bastien S <bastien.saussol@gmail.com>
2022-08-26 09:38:53 +0200

    package.json : add dev script server.js : server initialization

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const volleyball = require('volleyball');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 5008;
+
+mongoose.Promise = global.Promise;
+mongoose
+    .connect('mongodb+srv://test:test@cluster0.ipvsdny.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {console.log('####    MongoDb connected    ####');})
+    .catch(() => {console.log('Connection to db error');});
+
+app.use(cors());
+app.use(volleyball);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => console.log(`####    Server is running on port ${port} - work in progress    ####`));
