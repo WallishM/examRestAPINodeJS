97c0cc2efdf973fecc0d1ece811640744c998d2f
Bastien S <bastien.saussol@gmail.com>
2022-08-26 10:11:46 +0200

    Methods of product controller implemented -> to be used in route/product.js / no data validation -> TODO

---
+++ controllers/productController.js
@@ -0,0 +1,52 @@
+const Product = require('../models/product');
+
+exports.list = async (req, res) => {
+    try {
+        let products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.show = async (req, res) => {
+    try {
+        let product = await Product.find(req.params.id);
+        res.json(product);
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.store = async (req, res, next) => {
+    try {
+        // TODO: data validation
+        let product = new Product(req.body);
+        product = await product.save();
+        res.json({message: 'Product added', product});
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.update = async (req, res, next) => {
+    try {
+        // TODO: data validation
+        let product = await Product.findById(req.params.id);
+        Object.assign(product, req.body);
+        product = await product.save();
+        res.json({message: 'Product updated', product});
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.delete = async (req, res, next) => {
+    try {
+        let product = await Product.findById(req.params.id);
+        await product.delete();
+        res.json({lessage: 'Product deleted'});
+    } catch (err) {
+        next(err);
+    }
+};
